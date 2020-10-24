71639ef622ed5c094a57aad639e153eb247b4dc9
kim-taewoo <acoustic0419@gmail.com>
2020-10-24 16:17:25 +0900

    기본 jest 세팅 및 간단 작동 테스트

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = requrie('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+  }
+});
+
+module.exports = mongoose.model("Product", productSchema);
