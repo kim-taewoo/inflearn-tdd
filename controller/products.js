684f2df50a0b44d62942c746b3e26ce947a94144
kim-taewoo <acoustic0419@gmail.com>
2020-10-24 23:42:22 +0900

    Create 에 관한 유닛테스트 작성 부분 완료

--- controller/products.js
+++ controller/products.js
@@ -1,3 +1,6 @@
-exports.createProduct = (req, res) => {
-  res.send('안녕');
+const productModel = require('../models/Product');
+
+exports.createProduct = (req,res,next) => {
+  const createdProduct = productModel.create(req.body);
+  res.status(201).json(createdProduct);
 };
